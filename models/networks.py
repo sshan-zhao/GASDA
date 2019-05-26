bc0a0c1ee43d0a9ad30b7f0f343f339312377134
Shanshan Zhao <37661633+sshan-zhao@users.noreply.github.com>
2019-05-26 10:21:45 +1000

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -52,8 +52,6 @@ def get_norm_layer(norm_type='instance'):
         norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
     elif norm_type == 'instance':
         norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=True)
-    elif norm_type == 'synbatch':
-        norm_layer = functools.partial(SynchronizedBatchNorm2d, affine=True)
     elif norm_type == 'none':
         norm_layer = None
     else:
